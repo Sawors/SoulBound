dde0433710e753a0c47f6485d5ae28bf64e85968
Sawors <yt.sawors@gmail.com>
2023-08-08 18:18:29 +0200

    Initial commit

---
+++ src/main/java/io/github/sawors/soulbound/SoulBound.java
@@ -0,0 +1,17 @@
+package io.github.sawors.soulbound;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SoulBound extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
